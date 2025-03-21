7abd79c7805bca520fb2db5f791110eba496d4ea
wang1zhen <wang1zhendire@hotmail.com>
2025-03-21 16:29:18 +0900

    update custom-exaple.el

--- custom-example.el
+++ custom-example.el
@@ -1,23 +1,17 @@
-;;; custom.el --- user customization file    -*- lexical-binding: t no-byte-compile: t -*-
-
-;;; Code:
-
-(setq solarion-full-name "user name"
-      solarion-mail-address "user email address"
-      solarion-projectile-dir "projectile directory")
-
-(when (display-graphic-p)
-  (cond
-   ((equal (system-name) "") (setq font-size-factor 3))
-   (t (setq font-size-factor 2))))
-
+;; -*- mode: emacs-lisp; lexical-binding: t; -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(user-full-name "user name")
+ '(user-mail-address "user email address")
+ '(solarion-font-cjk "Noto Serif CJK SC")
+ '(solarion-font-default "CaskaydiaCove Nerd Font Mono")
+ '(solarion-font-emoji "Noto Color Emoji")
+ '(solarion-font-size 140)
+ '(solarion-font-variable "CaskaydiaCove Nerd Font")
  )
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
